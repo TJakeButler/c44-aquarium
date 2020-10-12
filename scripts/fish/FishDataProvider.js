e6aa43c0912e9e0a352bd6218f06ad6ad4156599
Jake Butler <tjakebutler@gmail.com>
2020-10-12 08:56:29 -0500

    Added new functions inside of FishDataProvider that will filter through fish that are holy, soilders and regular fish, imported to FishList. All working and automation is working

--- scripts/fish/FishDataProvider.js
+++ scripts/fish/FishDataProvider.js
@@ -50,7 +50,7 @@ const fishCollection = [
     {
         name: "Becky",
         species: "TigerFish",
-        length: 9,
+        length: 15,
         location: "California",
         food: "Veggies",
         image: "https://cf.ltkcdn.net/small-pets/images/std/246148-1600x1177-GettyImages-1050490488.jpg"
@@ -61,5 +61,40 @@ const fishCollection = [
 ]
 export const useFish = () => {
     return fishCollection.slice()
+}
+
+
+
+export const mostHolyFish = () => {
+    // 3, 6, 9, 12, etc... fish
+    const holyFish = []
+
+    for (const fish of fishCollection) {
+        if(fish.length % 3 === 0) {
+        holyFish.push(fish)
+        }
+    }
+    return holyFish
+}
+
+export const soldierFish = () => {
+    // 5, 10, 15, 20, 25, etc... fish
+    const soldiers = []
+    for (const fish of fishCollection) {
+        if(fish.length % 5 === 0 && fish.length % 3 !== 0)
+        soldiers.push(fish)
+    }
+    console.log(soldiers)
+    return soldiers
+}
+
+export const nonHolyFish = () => {
+    // Any fish not a multiple of 3 or 5
+    const regularFish = []
+    for (const fish of fishCollection) {
+        if(fish.length % 3 !== 0 && fish.length % 5 !== 0)
+        regularFish.push(fish)
+    }
+    return regularFish
 }
 
